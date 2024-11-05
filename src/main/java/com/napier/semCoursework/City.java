e6fc42d88ea06b5bd9ae8f408fa8516d08037ec1
amymillar <40589664@live.napier.ac.uk>
2024-11-05 17:16:11 +0000

    Update App with SQL statement to print all countries in descending population

---
+++ src/main/java/com/napier/semCoursework/City.java
@@ -0,0 +1,10 @@
+package com.napier.semCoursework;
+
+public class City
+{
+    public int id;
+    public String name;
+    public String countryCode;
+    public String district;
+    public int population;
+}
