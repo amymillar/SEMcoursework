68b9baeaac18deb7bd3052336d26d1cd5e2b0b1a
amymillar <40589664@live.napier.ac.uk>
2024-11-05 17:13:41 +0000

    Update App with SQL statement to print all countries in descending population

--- src/main/java/com/napier/semCoursework/App.java
+++ src/main/java/com/napier/semCoursework/App.java
@@ -1,5 +1,8 @@
 package com.napier.semCoursework;
+
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App
 {
@@ -10,6 +13,11 @@ public class App
 
         // Connect to database
         a.connect();
+
+        // Get all countries
+        List<Country> countries = a.getAllCountries();
+        // Display all countries
+        a.displayCountries(countries);
 
         // Disconnect from database
         a.disconnect();
@@ -76,6 +84,51 @@ public class App
             {
                 System.out.println("Error closing connection to database");
             }
+        }
+    }
+
+    public List<Country> getAllCountries()
+    {
+        List<Country> countries = new ArrayList<>();
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT name, population "
+                            + "FROM country "
+                            + "ORDER BY population DESC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            while (rset.next())
+            {
+                Country country = new Country();
+                country.name = rset.getString("name");
+                country.population = rset.getInt("population");
+
+                countries.add(country);
+            }
+            rset.close();
+            stmt.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+        }
+        return countries;
+    }
+
+    public void displayCountries(List<Country> countries)
+    {
+        for (Country country : countries)
+        {
+            System.out.println(
+                    "Name:" + country.name + "\n"
+                            + "Population:" + country.population + "\n");
         }
     }
 }
---
+++ src/main/java/com/napier/semCoursework/Country.java
@@ -0,0 +1,11 @@
+package com.napier.semCoursework;
+
+public class Country
+{
+    public String code;
+    public String name;
+    public String continent;
+    public String region;
+    public int population;
+}
+
